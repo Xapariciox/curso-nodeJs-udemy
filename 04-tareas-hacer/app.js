97877f125963d3840327e659e638970e57a6f350
roberto <rsandoval@takeaway.com>
2023-10-25 11:09:56 +0200

    avance tareas

---
+++ 04-tareas-hacer/app.js
@@ -0,0 +1,11 @@
+import * as colors from "colors";
+import { mostarMenu } from "./helpers/mensajes.js";
+console.clear();
+
+const main = async () => {
+  console.log("Hola Mundo");
+
+  mostarMenu();
+};
+
+main();
---
+++ 04-tareas-hacer/helpers/mensajes.js
@@ -0,0 +1,16 @@
+import * as color from "colors";
+
+export const mostarMenu = () => {
+  console.clear();
+  console.log("======================".green);
+  console.log("Seleccione una opción".green);
+  console.log("======================\n".green);
+
+  console.log(`${"1.".green} Crear tarea`);
+  console.log(`${"2.".green} Listar tareas`);
+  console.log(`${"3.".green} Listar tareas completadas`);
+  console.log(`${"4.".green} Listar tareas pendientes`);
+  console.log(`${"5.".green} Completar tarea(s)`);
+  console.log(`${"6.".green} Borrar tarea`);
+  console.log(`${"0.".green} Salir \n`);
+};
